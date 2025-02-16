6f2fd70218c16a7af73a781087297be2656eec83
Sercan KURBAN <115387811+SercanKurban96@users.noreply.github.com>
2025-02-16 19:52:14 +0300

    Order By Komutu

---
+++ 03-Gruplandırmalar ve Operatörler/SQLQuery06-OrderByKomutu.sql
@@ -0,0 +1,14 @@
+--TBLOGRENCILER tablosunda öðrencilerin adýný alfabetik sýraya göre A-Z gösteren sorgu
+--SELECT * FROM TBLOGRENCILER ORDER BY OGRAD ASC
+
+--TBLOGRENCILER tablosunda öðrencilerin adýný alfabetik sýraya göre Z-A gösteren sorgu
+--SELECT * FROM TBLOGRENCILER ORDER BY OGRAD DESC
+
+--TBLOGRENCILER tablosunda öðrencileri cinsiyet sýrasýna göre A-Z gösteren sorgu
+--SELECT * FROM TBLOGRENCILER ORDER BY OGRCINSIYET
+
+--TBLOGRENCILER tablosunda öðrencileri þehir sýrasýna göre A-Z gösteren sorgu
+--SELECT * FROM TBLOGRENCILER ORDER BY OGRSEHIR
+
+--TBLOGRENCILER tablosunda farklý kulüpler üzerinden alfabetik sýrasýna göre Z-A gösteren sorgu
+--SELECT DISTINCT OGRKULUP FROM TBLOGRENCILER ORDER BY OGRKULUP DESC
