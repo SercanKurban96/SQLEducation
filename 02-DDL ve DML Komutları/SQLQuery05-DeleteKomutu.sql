6a9367def690155a814311c9a8e906dd951ef171
Sercan KURBAN <115387811+SercanKurban96@users.noreply.github.com>
2025-02-16 17:30:24 +0300

    Delete Komutu

---
+++ 02-DDL ve DML Komutları/SQLQuery05-DeleteKomutu.sql
@@ -0,0 +1,8 @@
+--TBLDERSLER tablosunda þart saðlanmadýðý sürece tüm verileri silme komutu
+--DELETE FROM TBLDERSLER
+
+--TBLDERSLER tablosunda ID'ye göre þartlý silme iþlemi
+--DELETE FROM TBLDERSLER WHERE DERSID=6
+
+--TBLDERSLER tablosunda ders adýna göre þartlý silme iþlemi
+--DELETE FROM TBLDERSLER WHERE DERSAD='Algoritma'
