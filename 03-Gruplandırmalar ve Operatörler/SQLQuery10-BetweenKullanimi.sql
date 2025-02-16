f7d600b9871e6921357d1702d776bcee69443549
Sercan KURBAN <115387811+SercanKurban96@users.noreply.github.com>
2025-02-17 00:26:00 +0300

    Between Kullanımı

---
+++ 03-Gruplandırmalar ve Operatörler/SQLQuery10-BetweenKullanimi.sql
@@ -0,0 +1,8 @@
+--TBLNOTLAR tablosunda yer alan ortalamalarýn 74 ile 100 arasýnda olanlarý getiren sorgu
+--SELECT * FROM TBLNOTLAR WHERE ORTALAMA BETWEEN 74 AND 100
+
+--TBLOGRENCILER tablosunda yer alan öðrencinin ismi A'dan baþlayarak K'ya kadar olanlarý getiren sorgu
+--SELECT * FROM TBLOGRENCILER WHERE OGRAD BETWEEN 'A' AND 'K'
+
+--TBLOGRENCILER tablosunda yer alan öðrencinin ismi A'dan baþlayarak O'ya kadar olmayanlarý getiren sorgu
+--SELECT * FROM TBLOGRENCILER WHERE OGRAD NOT BETWEEN 'A' AND 'O'
