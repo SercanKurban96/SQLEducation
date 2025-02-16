0476d6223f94fc1f68c1d2af6e8fbd8caff67e02
Sercan KURBAN <115387811+SercanKurban96@users.noreply.github.com>
2025-02-17 00:13:38 +0300

    Top ve Percent Kullanımı

---
+++ 03-Gruplandırmalar ve Operatörler/SQLQuery08-Top&PercentKomutlari.sql
@@ -0,0 +1,11 @@
+--TBLOGRENCILER tablosunda üstten 3 kaydý getiren sorgu
+--SELECT TOP 3 * FROM TBLOGRENCILER
+
+--TBLOGRENCILER tablosunda üstten 4 kaydý getiren ancak ID sýrasýna göre tersten getiren sorgu
+--SELECT TOP 4 * FROM TBLOGRENCILER ORDER BY OGRID DESC
+
+--TBLOGRENCILER tablosunda bulunan kayýtlarýn yüzde 60'ýný getiren sorgu
+--SELECT TOP 60 PERCENT * FROM TBLOGRENCILER
+
+--TBLOGRENCILER tablosunda bulunan kayýtlarýn yüzde 81'ini getiren sorgu
+--SELECT TOP 81 PERCENT * FROM TBLOGRENCILER
