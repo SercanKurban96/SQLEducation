a0cee6a4494835feb4083b695a8097e88656c44b
Sercan KURBAN <115387811+SercanKurban96@users.noreply.github.com>
2025-02-16 17:02:51 +0300

    Select komutu uygulandı.

---
+++ 02-DDL ve DML Komutları/SQLQuery02-SelectKomutu.sql
@@ -0,0 +1,14 @@
+--TBLOGRENCILER tablosundaki tüm sütunlarý seçme
+--SELECT * FROM TBLOGRENCILER
+
+--TBLOGRENCILER tablosundaki OGRAD ve OGRSOYAD sütunlarýný seçme
+--SELECT OGRAD,OGRSOYAD FROM TBLOGRENCILER
+
+--TBLOGRENCILER tablosundaki þehirleri listeleme
+--SELECT OGRSEHIR FROM TBLOGRENCILER
+
+--TBLOGRENCILER tablosundaki bu kez ALÝ ismindeki öðrencileri listeleme
+--SELECT * FROM TBLOGRENCILER WHERE OGRAD='Ali'
+
+--TBLOGRENCILER tablosundaki ALÝ ismine ait þehirleri listeleme
+--SELECT OGRSEHIR FROM TBLOGRENCILER WHERE OGRAD='Ali'
