435fbeaa5ecffe4b5b116d0fc62b6d0b21db812e
Sercan KURBAN <115387811+SercanKurban96@users.noreply.github.com>
2025-02-16 16:51:25 +0300

    Create Alter Drop komutları uygulandı.

---
+++ 02-DDL ve DML Komutları/SQLQuery01-CreateAlterDropKomutlari.sql
@@ -0,0 +1,14 @@
+--TBLDERSLER isminde yeni bir tablo oluþturma
+--CREATE TABLE TBLDERSLER
+--(
+--DERSID tinyint,
+--DERSAD varchar(20)
+--)
+
+--TBLDERSLER tablosuna yeni sütun ekleme
+--ALTER TABLE TBLDERSLER
+--ADD KONTENJAN smallint
+
+--TBLDERSLER tablosundan sütun silme
+--ALTER TABLE TBLDERSLER
+--DROP COLUMN KONTENJAN
