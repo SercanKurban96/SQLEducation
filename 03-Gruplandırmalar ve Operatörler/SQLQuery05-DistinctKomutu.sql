3e900eee2a7b66adf1a135427a9dbe65b71df42f
Sercan KURBAN <115387811+SercanKurban96@users.noreply.github.com>
2025-02-16 19:46:31 +0300

    Distinct Komutu

---
+++ 03-Gruplandırmalar ve Operatörler/SQLQuery05-DistinctKomutu.sql
@@ -0,0 +1,9 @@
+--TBLOGRENCILER tablosunda farklý þehirleri listeleyen sorgu
+--SELECT DISTINCT OGRSEHIR FROM TBLOGRENCILER
+
+--TBLOGRENCILER tablosunda farklý þehirlerin sayýsýný getiren sorgu
+--SELECT COUNT(DISTINCT(OGRSEHIR)) FROM TBLOGRENCILER
+
+--TBLOGRENCILER tablosunda farklý kulüpleri listeleyen ve sayýsýný getiren sorgu
+--SELECT DISTINCT OGRKULUP FROM TBLOGRENCILER
+--SELECT COUNT(DISTINCT(OGRKULUP)) AS 'KULÜP SAYISI' FROM TBLOGRENCILER
