b6349b3eec29b208fd650ed3b02129398cee6a67
Sercan KURBAN <115387811+SercanKurban96@users.noreply.github.com>
2025-02-23 00:17:21 +0300

    Add files via upload

---
+++ 13-View/SQLQuery02-ViewOrnekleri.sql
@@ -0,0 +1,8 @@
+--View TEST2 (Þart: içinde a harfi geçen ürün adlarýný listeleyen sorgu)
+--SELECT * FROM TEST2
+
+--View TEST3 (Þart: Adet sayýsý 1'den büyük olanlarý listeleyen sorgu)
+--SELECT * FROM TEST3
+
+--Ödev: Þehri ADANA olan müþterilerin hareket tablosundaki deðerlerini gösteren sorguyu View üzerinden yapýn.
+--SELECT * FROM TEST3
