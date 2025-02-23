c30dfadbe88adb62fba5e473f00ee34c9f06ec16
Sercan KURBAN <115387811+SercanKurban96@users.noreply.github.com>
2025-02-23 13:36:13 +0300

    Add files via upload

---
+++ 14-Northwind Veri Tabanı/SQLQuery02-NorthwindSorguOrnekleri2.sql
@@ -0,0 +1,13 @@
+--NORTHWIND SORGU ÖRNEKLERÝ
+
+--View Oluþturma
+--CREATE VIEW VIEW1
+--AS
+--SELECT OrderID,CompanyName,FirstName + ' ' + LastName AS 'Employee',OrderDate,ShipName,ShipCity,ShipCountry FROM Orders
+--INNER JOIN Customers ON Customers.CustomerID = Orders.CustomerID
+--INNER JOIN Employees ON Employees.EmployeeID = Orders.EmployeeID
+
+--SELECT * FROM VIEW1
+
+--SELECT * FROM Invoices
+
