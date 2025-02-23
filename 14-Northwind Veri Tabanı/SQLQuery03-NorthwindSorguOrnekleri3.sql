84f0681cbb5581a21fd33f36c54ab053c4129169
Sercan KURBAN <115387811+SercanKurban96@users.noreply.github.com>
2025-02-23 13:54:06 +0300

    Add files via upload

---
+++ 14-Northwind Veri Tabanı/SQLQuery03-NorthwindSorguOrnekleri3.sql
@@ -0,0 +1,9 @@
+--NORTHWIND SORGU ÖRNEKLERÝ (VIEW)
+
+--SELECT * FROM VIEW2
+
+--ShipCity sütununda yer alan içinde arc harfi geçenleri listeleyen sorgu (View üzerinden yapýlacak)
+--SELECT * FROM VIEW2
+
+--Stored Procedures --> CustOrderHist çalýþtýrdýktan sonra yazýlan sorgu
+--EXEC CustOrderHist ANATR
