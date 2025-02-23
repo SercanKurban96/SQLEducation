3fa4356b2bfd9f431c8417124dad9c4547f9403f
Sercan KURBAN <115387811+SercanKurban96@users.noreply.github.com>
2025-02-23 15:49:25 +0300

    Add files via upload

---
+++ 15-T-SQL/SQLQuery05-TSQLPrintKullanimi.sql
@@ -0,0 +1,10 @@
+--T-SQL PRINT Kullanýmý
+
+--Print 'Merhaba'
+--DECLARE @s1 int, @s2 int, @sonuc int
+--set @s1=4
+--set @s2=8
+--set @sonuc=@s1*@s2
+--print '4 Ýle 8 Sayýlarýnýn Çarpýmý'
+--print '***************************'
+--print @sonuc
