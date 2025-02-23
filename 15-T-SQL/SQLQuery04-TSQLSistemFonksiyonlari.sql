849112d0b9ad4d2670daf71e9a41f4e63738bd91
Sercan KURBAN <115387811+SercanKurban96@users.noreply.github.com>
2025-02-23 15:10:27 +0300

    Add files via upload

---
+++ 15-T-SQL/SQLQuery04-TSQLSistemFonksiyonlari.sql
@@ -0,0 +1,6 @@
+--T-SQL Sistem Fonksiyonlarý
+
+--Bazý sistem fonksiyonlarý aþaðýda verilmiþtir. Sunucu adý, hata, varsayýlan dil gibi birçok sistem fonksiyonlarý mevcuttur, ancak SQL'de pek fazla kullaný
+--SELECT @@SERVERNAME, @@ERROR, @@DEFAULT_LANGID, @@LANGUAGE, @@MAX_CONNECTIONS, @@MICROSOFTVERSION, @@IDENTITY, @@TEXTSIZE, @@VERSION
+
+--USE SatisVT
